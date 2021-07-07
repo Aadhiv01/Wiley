7a0ecde150bd7a75f87b3071a6f70a2ad3125eeb
Aadhiv01 <84518908+Aadhiv01@users.noreply.github.com>
2021-07-07 12:11:24 +0530

    Update Employees.java

--- Employees.java
+++ Employees.java
@@ -3,6 +3,7 @@ package com.wiley.demo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class User{
 	String id;
@@ -38,51 +39,51 @@ public class Employees extends User{
 		this.project.name=pname;
 		this.project.budget=budget;
 	}
-	static double sum(List<Employees> l) {
-		double sum=0;
-		for(Employees e:l) {
-			sum+=e.salary;
-		}
-		return sum;
-	}
 	@Override
 	public String toString() {
 		return "Employee [id= " + id + ", name= " + name+", projectid= "+project.id+", project name= "+project.name+"]";
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ul.add(new User("1","Rahul"));
 		ul.add(new Employees("2","Vernon",1000,"Chennai","600020","1","Google",1500));
 		ul.add(new Employees("3","Joshua",2000,"Mumbai","600040","2","Wiley",3000));
 		ul.add(new Employees("4","Anwar",1200,"Delhi","600060","1","Google",1500));
-		ul.add(new Employees("5","Joshua",800,"Banglore","600080","2","Wiley",3000));
+		ul.add(new Employees("5","Chen",800,"Banglore","600080","2","Wiley",3000));
 		ul.add(new User("6","Rick"));
-		List<Employees> el = new ArrayList<>();
-		List<Employees> p1 = new ArrayList<>();
-		List<Employees> p2 = new ArrayList<>();
-		double b1=0,b2=0;
-		for(User u : ul) 
-			if(u.getClass().equals(Employees.class)) {
-				Employees e = (Employees)u;
-				if(e.salary>e.project.budget)
-					continue;
-				if(e.project.id.equals("1")) {
-					p1.add(e);
-					b1=e.project.budget;
-				}
-				else {
-					p2.add(e);
-					b2=e.project.budget;
-				}
-			}
-		while(sum(p1)>b1) {
+		List<Employees> p1 = ul.stream()
+								.filter(e -> e.getClass().equals(Employees.class))
+								.map(e->(Employees)e)
+								.filter(e->e.project.id.equals("1"))
+								.filter(e->(e.salary<=e.project.budget))
+								.collect(Collectors.toList());
+		List<Employees> p2 = ul.stream()
+								.filter(e -> e.getClass().equals(Employees.class))
+								.map(e->(Employees)e)
+								.filter(e->e.project.id.equals("2"))
+								.filter(e->(e.salary<=e.project.budget))
+								.collect(Collectors.toList());
+		double b1=p1.get(0).project.budget,b2=p2.get(0).project.budget;
+		int sum1 = (int)p1.stream()
+				  .mapToDouble(e->e.salary)
+				  .reduce(0,(a, b) -> a+b);
+		int sum2 = (int)p2.stream()
+				  .mapToDouble(e->e.salary)
+				  .reduce(0,(a, b) -> a+b);
+		while(sum1>b1) {
 			Collections.sort(p1,(l1,l2)->(int)(l1.salary-l2.salary));
 			p1.remove(p1.get(0));
+			sum1 = (int)p1.stream()
+					  .mapToDouble(e->e.salary)
+					  .reduce(0,(a, b) -> a+b);
 		}
-		while(sum(p1)>b2) {
-			Collections.sort(p1,(l1,l2)->(int)(l1.salary-l2.salary));
-			p1.remove(p2.get(0));
+		while(sum2>b2) {
+			Collections.sort(p2,(l1,l2)->(int)(l1.salary-l2.salary));
+			p2.remove(p2.get(0));
+			sum2 = (int)p2.stream()
+					  .mapToDouble(e->e.salary)
+					  .reduce(0,(a, b) -> a+b);
 		}
+		List<Employees> el = new ArrayList<>();
 		el.addAll(p1);
 		el.addAll(p2);
 		for(Employees e:el)
