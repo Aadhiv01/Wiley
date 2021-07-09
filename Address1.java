4d8fef011f6e1ab08c546d4e2d4706b402e8b1f4
Aadhiv01 <84518908+Aadhiv01@users.noreply.github.com>
2021-07-09 12:07:32 +0530

    Add files via upload

---
+++ Address1.java
@@ -0,0 +1,64 @@
+package com.wiley.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Address1 {
+	String city;
+	String zipcode;
+	public Address1(String city, String zipcode) {
+		super();
+		this.city = city;
+		this.zipcode = zipcode;
+	}
+	@Override
+	public String toString() {
+		return "Address1 [city=" + city + ", zipcode=" + zipcode+"]";
+	}
+	public static void main(String[] args) {
+		List<User2> ul = new ArrayList<>();
+		ul.add(new User2(2,"User2",Arrays.asList(new Address1("Delhi","110001"),new Address1("Banglore","560001"))));
+		ul.add(new User2(1,"User1",Arrays.asList(new Address1("Chennai","600020"),new Address1("Banglore","560001"))));
+		ul.add(new User2(3,"User3",Arrays.asList(new Address1("Chennai","600020"),new Address1("Banglore","560001"))));
+		HashMap<Address1,List<User2>> hash = new HashMap<>();
+		for(User2 u : ul) {
+			for(int i=0;i<u.ad.size();i++) {
+				final int x =i;
+				Optional<Address1> a = hash.keySet().stream().filter(s->s.city.equals(u.ad.get(x).city)).findAny();
+				if(!a.isPresent()) {
+					hash.put(new Address1(u.ad.get(i).city,u.ad.get(i).zipcode),new ArrayList<>());
+					a=hash.keySet().stream().filter(s->s.city.equals(u.ad.get(x).city)).findAny();
+				}
+				hash.get(a.get()).add(new User2(u.id,u.name));
+			}
+		}
+		for(Address1 a : hash.keySet().stream().filter(s->hash.get(s).size()==1).collect(Collectors.toSet()))
+			hash.remove(a);
+		hash.forEach((k,v)->System.out.println(k+" - "+v));
+	}
+
+}
+class User2{
+	int id;
+	String name;
+	List<Address1> ad = new ArrayList<>();
+	public User2(int id, String name, List<Address1> ad) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.ad = ad;
+	}
+	public User2(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "User2 [id=" + id + ", name=" + name + "]";
+	}
+}
