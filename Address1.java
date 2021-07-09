1857ebf927f0deb6f5d0b096340b92c12e22f5b9
Aadhiv01 <84518908+Aadhiv01@users.noreply.github.com>
2021-07-09 15:59:53 +0530

    Update Address1.java

--- Address1.java
+++ Address1.java
@@ -2,9 +2,12 @@ package com.wiley.demo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class Address1 {
@@ -37,9 +40,22 @@ public class Address1 {
 		}
 		for(Address1 a : hash.keySet().stream().filter(s->hash.get(s).size()==1).collect(Collectors.toSet()))
 			hash.remove(a);
-		hash.forEach((k,v)->System.out.println(k+" - "+v));
+		//Sorted by number of users
+		System.out.println("--------------------------------------------------------------Sorted by number of users------------------------------------------------\n");
+		List<Address1> st = hash.keySet().stream().collect(Collectors.toList());
+		Collections.sort(st,(a,b)->hash.get(a).size()-hash.get(b).size());
+		st.forEach(l->System.out.println(l+" - "+hash.get(l)));
+		System.out.println("\n--------------------------------------------------------------Sorted by City-----------------------------------------------------------\n");
+		//Sorted by City
+		TreeMap<Address1,List<User2>> tm1 = new TreeMap<>(new sortByCity());
+		tm1.putAll(hash);
+		tm1.forEach((k,v)->System.out.println(k+" - "+v));
+		System.out.println("\n--------------------------------------------------------------Sorted by Zipcode---------------------------------------------------------\n");
+		//Sorted by Zipcode
+		TreeMap<Address1,List<User2>> tm2 = new TreeMap<>(new sortByZipcode());
+		tm2.putAll(hash);
+		tm2.forEach((k,v)->System.out.println(k+" - "+v));
 	}
-
 }
 class User2{
 	int id;
@@ -60,4 +76,20 @@ class User2{
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + "]";
 	}
+}
+class sortByCity implements Comparator<Address1>{
+
+	@Override
+	public int compare(Address1 o1, Address1 o2) {
+		return o1.city.compareTo(o2.city);
+	}
+	
+}
+class sortByZipcode implements Comparator<Address1>{
+
+	@Override
+	public int compare(Address1 o1, Address1 o2) {
+		return o1.zipcode.compareTo(o2.zipcode);
+	}
+	
 }
