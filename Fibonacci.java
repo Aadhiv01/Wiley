0f2a99a8df346d316aee856da862e02f1b281e6d
Aadhiv01 <84518908+Aadhiv01@users.noreply.github.com>
2021-07-14 11:59:40 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,59 +3,65 @@ package com.wiley.demo;
 import java.util.*;
 
 public class Fibonacci {
-
+	Fibonacci f = new Fibonacci();
 	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
 		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
-		int n = 5;
+		int n = 8;
 		FiboThread ft = new FiboThread(n);
 		Thread t = new Thread(ft);
-		t.setName("Thread 1");
-		t.setPriority(Thread.MAX_PRIORITY);
 		Thread t1 = new Thread(ft);
+		t.setName("Thread 1");
+		t.setPriority(10);
 		t.setName("Thread 2");
-		t1.setPriority(Thread.MIN_PRIORITY);
+		t1.setPriority(10);
 		t.start();
 		t1.start();
-		sc.close();
 //		System.out.println(ft.getSum());
 //		ft.getL().forEach(System.out::println);
+		sc.close();
 	}
 }
 
 class FiboThread implements Runnable{
-	private int sum;
-	int n;
-	int x = 0;
+	private int sum = 0;
+	private int n;
+	int k = 0;
+	int a = 0;
+	int b = 1;
+	int c = 0;
 	private List<String> l = Collections.emptyList();
 	public void run() {
-			//System.out.println(Thread.currentThread().getName());
 			l = new ArrayList<>();
-			sum=0;
-			int a = 0, b = 1, c=0;
-			for(int i=0;i<n;i++) {
-				c=a+b;
-				a=b;
-				b=c;
+			while(true) {
+				if(l.size()==n)
+					break;
 				try {
-				if(Thread.currentThread().getName().equals("Thread-1")) {
-					sum+=a;
-					if(i==n-1)
-						System.out.println(sum);
-					Thread.sleep(500);
-				}
-				else {
-					String s = x+" + "+a+" = "+(x+a);
-					x+=a;
-					l.add(s);
-					if(i==n-1) {
-						l.forEach(System.out::println);
+					if(Thread.currentThread().getName().equals("Thread-1")) {
+						if(l.size()==k) {
+							c=a+b;
+							a=b;
+							b=c;
+							sum+=a;
+							k+=1;
+						}
+						Thread.sleep(50);
 					}
-					Thread.sleep(100);
-				}
+					else {
+						if(l.size()==k-1) {
+							String s = (sum-a)+" + "+a+" = "+(sum);
+							l.add(s);
+							if(l.size()==n) {
+								System.out.println(sum);
+								l.forEach(System.out::println);
+							}
+						}
+						Thread.sleep(50);
+					}
 				} catch (InterruptedException e) {
 					e.printStackTrace();}
 			}
+
 	}
 	FiboThread(int n){
 		this.n=n;
