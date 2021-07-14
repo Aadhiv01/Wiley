5aa0f358f48d0e5eca162d2d5334bd00187e79df
Krish <Krish@DESKTOP-B9VPEB4>
2021-07-14 13:16:35 +0530

    Exception

---
+++ Demo/src/com/wiley/demo/ExceptionEg.java
@@ -0,0 +1,21 @@
+package com.wiley.demo;
+
+public class ExceptionEg {
+
+	
+	public static void main(String[] args) throws InterruptedException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+//		 int[] arr = {10,1,20,50};
+//		 for(int i : arr) {
+//			 try {
+//				 if(i==1)
+//					 throw new ArithmeticException("Division by 1");
+//				 System.out.println(100/i);
+//			 }
+//			 catch(ArithmeticException e) {
+//				 System.out.println(e.getMessage());
+//			 }
+//		 }
+	}
+
+}
